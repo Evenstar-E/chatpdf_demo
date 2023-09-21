b06e07d741bd914f9a6a5fe56ed77a1f25604305
Evenstar-E <84828777+Evenstar-E@users.noreply.github.com>
2023-09-21 23:11:03 +0800

    Update chatpdf_1.py

--- chatpdf_1.py
+++ chatpdf_1.py
@@ -14,7 +14,6 @@ from langchain.chat_models import ChatOpenAI
 import base64
 from pathlib import Path
 import tempfile
-from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
 
 
 st.set_page_config(page_title="ChatPDF",page_icon="🧊",layout="wide")
